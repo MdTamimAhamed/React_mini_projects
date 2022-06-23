f637f31dc07103096f4845eb67fc9cd9836a7bf5
MdTamimAhamed <tamimssgts999@gmail.com>
2022-06-24 01:18:25 +0600

    card project completed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import CardData from './Components/Card/CardData';
 function App() {
   return (
     <>
+      <div className='card-header'>
+        <div className='container'>
+            <h1 className='list-title'>Movie Cards </h1>
+        </div>
+      </div>
       <div className='card-section'>
         <div className='container'>
           <div className='card-wrapper'>
-            {CardData.map(function movies(value){
+            {CardData.map((value) =>{
                 return (
                   <Card
+                    key = {value.id}
                     imgSrc={value.imgSrc}
                     availability={value.availability}
                     imgTitle={value.imgTitle}
--- src/Components/Card/CardData.jsx
+++ src/Components/Card/CardData.jsx
@@ -9,48 +9,56 @@ import thor from '../../images/thor.jpg';
 
 const CardData = [
     {
+        id: 1,
         imgSrc:  shangchi ,
         availability: "Available now!",
         imgTitle: "Shang-chi",
         movLink: "https://www.marvel.com/movies/shang-chi-and-the-legend-of-the-ten-rings"
     },
     {
+        id: 2,
         imgSrc: avengers,
         availability: "Available now!",
         imgTitle: "Avengers",
         movLink: "https://www.imdb.com/title/tt4154796/"
     },
     {
+        id: 3,
         imgSrc: spiderman,
         availability: "Coming soon!",
         imgTitle: "Spider-man",
         movLink: "https://www.marvel.com/movies/spider-man-no-way-home"
     },
     {
+        id: 4,
         imgSrc: topgun,
         availability: "Available now!",
         imgTitle: "Top Gun",
         movLink: "https://www.imdb.com/title/tt1745960/"
     },
     {
+        id: 5,
         imgSrc: thor,
         availability: "Available now!",
         imgTitle: "Thor",
         movLink: "https://www.imdb.com/title/tt1745960/"
     },
     {
+        id: 6,
         imgSrc: avatar,
         availability: "Coming soon!",
         imgTitle: "Avatar 2",
         movLink: "https://www.imdb.com/title/tt1745960/"
     },
     {
+        id: 7,
         imgSrc: blackadam,
         availability: "Coming soon!",
         imgTitle: "Black Adam",
         movLink: "https://www.imdb.com/title/tt1745960/"
     },
     {
+        id: 8,
         imgSrc: doctor,
         availability: "Available now",
         imgTitle: "Doctor Strange",
